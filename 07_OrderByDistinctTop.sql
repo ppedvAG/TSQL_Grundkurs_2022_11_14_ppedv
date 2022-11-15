b2f0a9e128e0a2f3a7761dc6a80ab504f81be439
ppedvLukasK <lukask@ppedv.de>
2022-11-15 10:41:02 +0100

    M007

---
+++ 07_OrderByDistinctTop.sql
@@ -0,0 +1,8 @@
+USE Northwind;
+
+SELECT * FROM Customers ORDER BY Country; --ORDER BY: Ergebnis nach einer Spalte sortieren
+
+SELECT * FROM Customers ORDER BY Country, City; --Sortierung nach mehreren Spalten (Primär: Country, Sekundär: City)
+
+SELECT * FROM Customers ORDER BY Country DESC, City ASC; --ASC/DESC: Sortierrichtung vorgeben (DESC -> Absteigend, ASC -> Aufsteigend, Standard)
+
